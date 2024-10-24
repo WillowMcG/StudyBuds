277c40b3af2ea79a85f460a7af61bbaa419651a8
ethan-105 <ethan.r.pena@gmail.com>
2024-10-24 11:08:34 -0400

    Added functions to access Firebase Realtime Database through Backend REST API. Added multiple page functionality to Frontend.

---
+++ frontend/src/pages/signup.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// setup taken from https://www.geeksforgeeks.org/how-to-create-a-multi-page-website-using-react-js/
+
+const Signup = () => {
+    return (
+        <div>
+            <h1>
+                This is the signup page, congrats!
+            </h1>
+        </div>
+    );
+};
+
+export default Signup;
